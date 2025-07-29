a95c88996b53e5fbbc00e04f3c28b35258f986e2
Nemog17 <nehemuelgarcia@gmail.com>
2025-07-28 22:45:29 -0400

    Fix missing closing parenthesis in home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -262,8 +262,9 @@ class _HomePageState extends State<HomePage> {
                             const SizedBox(height: 8),
                           ],
                         ),
-                      );
-                    },
+                      ),
+                    );
+                  },
                   ),
                 ),
               ],
